a46ae27fcd451508b125063e0d618721516c17a1
Tim Emmert <tim.emmert@tngtech.com>
2021-11-23 19:10:09 +0100

    Add graal proof of concept test function.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,20 @@
+import json
+
 from perceval.backends.core.git import Git
 
-from src.cli import CLIArguments, build_arguments
-from src.data_destination import dump_repo_to_elastic_search, dump_repo_to_json
+from cli import CLIArguments, build_arguments
+from data_destination import dump_repo_to_elastic_search, dump_repo_to_json
 
 GITPATH_TMP = "/tmp/ppp"
+
+
+def graal_testfunction(arguments: CLIArguments):
+    """ Proof of concept implementation """
+    from graal.backends.core.cocom import CoCom
+    repo_uri = arguments.repo_path
+    cc = CoCom(uri=repo_uri, git_path=GITPATH_TMP)
+    for c in cc.fetch():
+        print(json.dumps(c, indent=4))
 
 
 def main(arguments: CLIArguments):
@@ -13,6 +24,7 @@ def main(arguments: CLIArguments):
 
     if arguments.dump_to_elastic_search:
         dump_repo_to_elastic_search(repo=repo)
+    graal_testfunction(arguments)
 
 
 if __name__ == "__main__":
