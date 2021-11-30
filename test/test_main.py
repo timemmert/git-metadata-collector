91e38e3cfb686d2c17920180f27a88cc0d1efd16
Tim Emmert <tim.emmert@tngtech.com>
2021-11-30 11:30:51 +0100

    Adapt integration test to bugfix.

--- test/test_main.py
+++ test/test_main.py
@@ -22,7 +22,10 @@ def test_basic_dump(percival_repo):
     main(arguments=CLIArguments(scan_path=percival_repo, out_folder=results_folder))
 
     repo_name = percival_repo.replace(os.sep, "_")
+    if repo_name[0] == "_":
+        repo_name = repo_name[1:]
     results_file = os.path.join(results_folder, f"{repo_name}_metadata.json")
+
     assert os.path.isfile(results_file)
     with open(results_file, "r") as fp:
         assert json.load(fp)[0]["message"] == "Initial import"
