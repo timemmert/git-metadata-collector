aeee1f27eb96d692d6e1cbf74628a9a15eb59ec9
Tim Emmert <tim.emmert@tngtech.com>
2021-11-30 12:40:52 +0100

    Refactor dockerfile to be more readable.

--- dockerize.sh
+++ dockerize.sh
@@ -2,3 +2,9 @@
 
 docker build -t base-metadata-collector -f docker/Dockerfile-base .
 docker build -t git-metadata-collector -f docker/Dockerfile .
+
+docker run \
+  -v /home/tng/Documents/facebook/grimoirlab/git-metadata-collector/out:/analysis \
+  -v /home/tng/Documents/facebook/grimoirlab/git-metadata-collector/demo:/scan_folder \
+  git-metadata-collector \
+  --scan-path /scan_folder --out-folder /analysis
